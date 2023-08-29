1bc3546e6c1c82faaea0c0ba831b4e76cff80cde
52fe1243c08f <donnelmuga@gmail.com>
2023-08-29 00:59:09 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *mktemp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		mktemp = (*head)->next;
+		free(*head);
+		*head = mktemp;
+	}
+
+	*head = NULL;
+}
+
+
